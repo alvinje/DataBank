918e68f08ee2091dd90259c0ae90d76eff9172bd
Jérémie Alvin <je.alvin@orange.fr>
2018-04-19 15:26:28 +0200

    Read and adapt data

---
+++ scripts/script.R
@@ -0,0 +1,18 @@
+setwd("D:/logiciel/RStudio/save/DataBank/data")
+csv <- read.csv("banking_credit.csv", T, sep=';')
+is.data.frame(csv)
+filteredCsv <- csv[csv$Typologie_client != "",]
+
+View(filteredCsv)
+
+# Update csv data
+filteredCsv <- droplevels(filteredCsv)
+summary(filteredCsv)
+boxplot(filteredCsv$Montant_credit)
+
+# A verifier:
+# bon nb de ligne, de collones, var bien typées (numeric, qualitatives)
+# bug à la lecture -> verifier si aucunes données se sont glissées en trop
+
+# - supprimer les données corrrompues ou abérentes boxplot
+# - créer les models
