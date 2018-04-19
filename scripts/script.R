20c6d22475bcc14ace2372429667bb81546a2412
KevinPhomm <kevin.phommavanh@group-hpi.com>
2018-04-19 15:48:04 +0200

    Remove absurd data Montant_credit

--- scripts/script.R
+++ scripts/script.R
@@ -12,9 +12,16 @@ filteredCsv <- droplevels(filteredCsv)
 summary(filteredCsv)
 boxplot(filteredCsv$Montant_credit)
 
+# Remove absurd data Montant_credit
+filteredCsv = filteredCsv[!filteredCsv$Montant_credit %in% boxplot(filteredCsv$Montant_credit,range=2)$out,]
+
+
+
+
 # A verifier:
 # bon nb de ligne, de collones, var bien typées (numeric, qualitatives)
 # bug à la lecture -> verifier si aucunes données se sont glissées en trop
 
 # - supprimer les données corrrompues ou abérentes boxplot
 # - créer les models
+
