ce7d30446e2240144485511a65321b43d4f699b0
Jérémie Alvin <je.alvin@orange.fr>
2018-05-24 10:34:14 +0200

    Convert vars and start create model

--- scripts/script.R
+++ scripts/script.R
@@ -6,25 +6,45 @@ csv <- read.csv("banking_credit.csv", T, sep=';')
 is.data.frame(csv)
 filteredCsv <- csv[csv$Typologie_client != "",]
 
-View(filteredCsv)
-
-# Update csv data
-filteredCsv <- droplevels(filteredCsv)
-filteredCsv$Id <- NULL
 
 # Remove absurd data Montant_credit
-filteredCsv = filteredCsv[!filteredCsv$Montant_credit %in% boxplot(filteredCsv$Montant_credit,range=2)$out,]
+filteredCsv <- filteredCsv[!filteredCsv$Montant_credit %in% boxplot(filteredCsv$Montant_credit,range=2)$out,]
 
 # calcul de l'age
 filteredCsv$Date_naissance <- as.Date(filteredCsv$Date_naissance, format="%d/%m/%Y")
 now <- Sys.Date()
 filteredCsv$age <- as.integer(difftime(now, filteredCsv$Date_naissance, unit="weeks")/52.25)
 
+# Convert data quali to quanti
+filteredCsv$Nb_credit_existants <- as.integer(filteredCsv$Nb_credit_existants)
+filteredCsv$Garants <- as.integer(filteredCsv$Garants)
+
+# Update csv data
+filteredCsv <- droplevels(filteredCsv)
+filteredCsv$Id <- NULL
+filteredCsv$Date_naissance <- NULL
+filteredCsv$Sexe <- NULL
+filteredCsv$Id <- NULL
+
+View(filteredCsv)
 summary(filteredCsv)
 boxplot(filteredCsv$Montant_credit)
 
+# Create test and apprentissage vars
+set.seed(1) # Pour que l'on ait tous les mêmes échantillons
 
+n.app=2/3*nrow(filteredCsv)
 
+n.test=1/3*nrow(filteredCsv)
+
+ind.app = sample(1:nrow(filteredCsv),size=n.app,replace=FALSE)
+
+echan.app = filteredCsv[ind.app,]
+
+echan.test = filteredCsv[-ind.app,]
+
+# création du modele
+reg.model3 = step(echan.app,direction="both")
 
 # A verifier:
 # bon nb de ligne, de collones, var bien typÃ©es (numeric, qualitatives)
